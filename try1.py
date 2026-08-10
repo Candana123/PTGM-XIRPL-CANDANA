d87a256daa51d954d7578d6647e78f7d0292f044
Candana123 <candanaxlrpl@gmail.com>
2026-08-10 17:41:30 +0700

    Add biodata printing script in try1.py

---
+++ try1.py
@@ -0,0 +1,12 @@
+nama = "septian"
+umur = 19 
+hobi = "tidur"
+laki = "true"
+badan = 58.5
+
+print("====biodata====")
+print("nama:", nama)
+print("umur:", umur,"tahun")
+print("hobi:", hobi)
+print("lakilaki:", laki)
+print("berat badan:", badan)
