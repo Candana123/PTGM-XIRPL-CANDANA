7f1a9767c406d57a187dd56b466528477ea529cd
Candana123 <candanaxlrpl@gmail.com>
2026-08-18 12:52:33 +0700

    Add bitwise operations in tugas2.py

---
+++ tugas2.py
@@ -0,0 +1,9 @@
+a=108
+b=183
+c=41
+
+print("hasil dari no 1:", c&a)
+print("hasil dari no 2:", b|a)
+print("hasil dari no 3:", b^c)
+print("hasil dari no 1:", a&b|c)
+print("hasil dari no 4:", c^a^b)
